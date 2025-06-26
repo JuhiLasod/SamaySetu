3cf65210ed6480f3201dde7573ce0597956f316d
JuhiLasod <juhilasod29@gmail.com>
2025-06-26 17:14:03 +0530

    login ui done

---
+++ frontend/lib/screens/login.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+
+class login extends StatefulWidget{
+  const login({super.key});
+  @override
+  State <login> createState()=> _loginState(); 
+}
+
+class _loginState extends State<login>
+{
+  final TextEditingController _emailController=TextEditingController();
+  final TextEditingController _passController=TextEditingController();
+  // final secreenHeight=MediaQuery.of(context).size.height;
+  @override
+  Widget build(BuildContext context)
+  {
+    final screenHeight=MediaQuery.of(context).size.height;
+    final screenWidth=MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Stack(
+          children:[ Column(children:[
+            Container(
+              height: screenHeight * 0.5,
+              color: Color.fromARGB(255, 0, 0, 0),
+            ),
+            Container(
+              height: screenHeight * 0.5,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors:[ Color.fromARGB(255, 111, 180, 253),Color.fromARGB(255, 0, 0, 0)],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter
+                )
+              ),
+            )
+          ] ),
+          
+            Positioned(
+              top: screenHeight *0.2,
+              left: (screenWidth * 0.25),
+              child: Image.asset('assets/images/logo.png',
+                width: screenWidth * 0.5,
+                // height: ,
+                fit: BoxFit.cover,
+              ),
+            ),
+          Positioned(
+            top: screenHeight * 0.35,
+            left: screenWidth * 0.22,
+            child: Text('SamaySetu',style: TextStyle(fontFamily: 'title',fontSize: 50, color: Color.fromARGB(255, 255, 255, 255)),)
+          ),
+          Positioned(
+              top: screenHeight *0.45,
+              left: screenWidth *0.1,
+              child: Container(
+                width: screenWidth * 0.8,
+                height: screenHeight * 0.4,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  borderRadius: BorderRadius.circular(25)
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // Padding(
+                        //   padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                        //   child: Text('Email',style: 
+                        //     TextStyle(
+                        //       color: Color.fromARGB(255, 0, 0, 0),
+                        //       fontSize: 20
+                        //     ),
+                        //   )
+                        // ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(0,0,0,20),
+                          child: TextFormField(
+                            controller: _emailController,
+                            decoration: InputDecoration(
+                              hintText: 'Enter your registered email id',
+                              hintStyle: TextStyle(color: Color.fromARGB(100, 0, 0, 0)),
+                              border: OutlineInputBorder(),
+                              
+                              ),
+                            ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(0,0,0,20),
+                          child: TextFormField(
+                            controller: _passController,
+                            decoration: InputDecoration(
+                              hintText: 'Enter password',
+                              hintStyle: TextStyle(color: Color.fromARGB(100, 0, 0, 0)),
+                              border: OutlineInputBorder(),
+                              
+                              ),
+                            ),
+                        ),
+                        ],
+                    ),
+                    Column(
+                    children: [
+                      SizedBox(
+                        width: 200,
+                        height: 50,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)
+                            ),
+                            backgroundColor: Color.fromARGB(255, 0, 0, 0)
+                          ),
+                          onPressed: (){}, 
+                          child: Text('Sign in',
+                            style: TextStyle(fontSize: 15, color: Color.fromARGB(255, 255, 255, 255)),
+                          )
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsGeometry.fromLTRB(0, 20,0, 0),
+                        child: GestureDetector(
+                          child: Text('forgot password?',
+                            style: TextStyle(decoration: TextDecoration.underline,fontSize: 15 ),
+                            ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsGeometry.fromLTRB(0, 20,0, 0),
+                        child: GestureDetector(
+                          child: Text('New to SamaySetu? Sign up now.',
+                            style: TextStyle(decoration: TextDecoration.underline,fontSize: 15 ),
+                            ),
+                        ),
+                      )
+                    ]
+                  )
+                  ]),
+                  // Column(
+                  //   children: [
+                  //     ElevatedButton(
+                  //       onPressed: (){}, 
+                  //       child: Text('Sign in')
+                  //     )
+                  //   ]
+                  // )
+                ),
+              )
+          ),
+              
+        ] ),
+
+    
+    );
+  }
+}
