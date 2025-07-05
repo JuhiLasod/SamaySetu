3535d8867d4937db19d614119c9137d54b7cf9ce
JuhiLasod <juhilasod29@gmail.com>
2025-07-05 08:01:50 +0530

    profiles backend done

--- backend/controllers/editProfileController.js
+++ backend/controllers/editProfileController.js
@@ -2,10 +2,25 @@ import Profiles from "../models/Profiles.js";
 const editProfileController=async(req,res)=>{
     try{
     // console.log('yay');
-    const {email,_profileImage,name,_selectedGender,bio,no,myServices}=req.body;
+    const {email,_profileImage,name,_selectedGender,bio,no}=req.body;
+    const myServices = JSON.parse(req.body.myServices);
     // console.log('ok');
     // console.log(email,_profileImage,name,_selectedGender,bio,no,myServices);
-    const done= await new Profiles({email,name,no,gender: _selectedGender, bio,balance:0,myskills:myServices,history: [], dp:_profileImage})
+    const done= await new Profiles({
+        email,
+        name,
+        no,
+        gender: _selectedGender, 
+        bio,
+        balance:0,
+        myskills:myServices,
+        history: [], 
+        dp: req.file?
+            {
+                data: req.file.buffer,
+                contentType: req.file.mimeType
+            }:undefined
+        });
     await done.save();
     if(done)
     {
