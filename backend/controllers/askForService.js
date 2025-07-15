dbbb0c6ca3228d03447ef28a1e1bcdc742554c13
JuhiLasod <juhilasod29@gmail.com>
2025-07-15 17:17:12 +0530

    my requests backend done

--- backend/controllers/askForService.js
+++ backend/controllers/askForService.js
@@ -1,12 +1,16 @@
 import nodemailer from 'nodemailer';
 import dotenv from "dotenv";
+import Requests from '../models/Requests.js';
 import { text } from 'express';
 
 dotenv.config();
 const askForService=async(req,res)=>{
     
-        const {email,datetime,place}=req.body;
-        console.log(email);
+        const {from, service, to,datetime,place}=req.body;
+        console.log("saving below content");
+        console.log(from);
+        console.log(service);
+        console.log(to);
         console.log(datetime);
         console.log(place);
         const transporter=nodemailer.createTransport({
@@ -19,14 +23,23 @@ const askForService=async(req,res)=>{
         console.log("trns created");
         const mailOptions={
             from: process.env.MAIL_ID,
-            to: email,
+            to: to,
             subject:"asking for your availibility",
-            text:`mail from SamaySetu date time is ${datetime} and place is ${place}`
+            text:`${from} asked for service to ${to} for ${service}mail from SamaySetu date time is ${datetime} and place is ${place}`
         }
         try{
             console.log("insode try");
         await transporter.sendMail(mailOptions);
+
         console.log("mail succ sent");
+        try{
+        const newreq= new Requests({from,to, service, datetime, place});
+        await newreq.save();
+        console.log("saved in req db");
+        }catch(e)
+        {
+            console.log("couldnt save in req db");
+        }
         res.sendStatus(200);
     } 
     catch(e)
