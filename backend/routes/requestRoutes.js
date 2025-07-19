725606d7662877e7c79b66bc3cb95b1dc881199d
JuhiLasod <juhilasod29@gmail.com>
2025-07-19 08:52:22 +0530

    mad done

--- backend/routes/requestRoutes.js
+++ backend/routes/requestRoutes.js
@@ -2,12 +2,14 @@ import express from 'express';
 import myRequests from '../controllers/myRequests.js';
 import reqToMe from "../controllers/reqToMe.js";
 import reqStatus from "../controllers/reqStatus.js";
+import MADController from '../controllers/MADController.js';
 
 const requestRoutes=express.Router();
 
 requestRoutes.post("/my-req",myRequests);
 requestRoutes.post("/req-to-me",reqToMe);
 requestRoutes.post("/set-status",reqStatus);
+requestRoutes.post("/set-mad",MADController);
 
 
 export default requestRoutes;
