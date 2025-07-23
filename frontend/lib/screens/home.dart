19ce6006ddc96f3065381068f56b9b02908d138c
JuhiLasod <juhilasod29@gmail.com>
2025-07-23 23:01:58 +0530

    dash ui

--- frontend/lib/screens/home.dart
+++ frontend/lib/screens/home.dart
@@ -7,6 +7,8 @@ import "./myRequests.dart";
 import "./reqToMe.dart";
 import './explore.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+
 class home extends StatefulWidget
 {
   const home({super.key});
@@ -55,13 +57,26 @@ class _homeState extends State<home>
       Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>editProfile()));
     }
   }
-
-
+  int balance=0;
+  void loadContent()async{
+  final prefs=await SharedPreferences.getInstance();
+  final email=await prefs.getString('email');
+  final res= await http.post(Uri.parse("http://10.0.2.2:8000/profile/loadprofile"),
+  headers:{'Content-Type':'application/json'},
+  body:jsonEncode({'email':email})
+  );
+  final data=jsonDecode( res.body);
+  setState(() {
+    // name=data['name'];
+    balance=data['balance'];
+    });
+  }
   
   @override
   void initState()
   {
     super.initState();
+    loadContent();
     print("inside init");
     WidgetsBinding.instance.addPostFrameCallback((_) {
       isProfile();
@@ -73,49 +88,111 @@ class _homeState extends State<home>
   }
   Widget build(BuildContext context)
   {
-    
+    final screenHeight= MediaQuery.of(context).size.height;
+    final screenWidth= MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(50),
-            child:ElevatedButton(
-              onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>explore()));},  
-              child: Text('explore')
-            )
-          ),
-
-          Padding( 
-            padding: EdgeInsets.all(50),
-            child: ElevatedButton(
-              onPressed: handleLogout, 
-              child: Text('logout')
-            )
-          ),
-          
-          Padding( 
-            padding: EdgeInsets.all(50),
-            child: ElevatedButton(
-              // onPressed: handleMyReq,
-              onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>myRequests()));}, 
-              child: Text('My Requests')
-            )
-          ),
-
-          Padding( 
-            padding: EdgeInsets.all(50),
-            child: ElevatedButton(
-              // onPressed: handleMyReq,
-              onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>reqToMe()));}, 
-              child: Text('Requests to me')
-            )
-          ),
-
-          ElevatedButton(
-            onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>editProfile()));}, 
-            child: Text('edit profile')
-          )
-        ]
+      body: Container(
+        color: Colors.black,
+        child: Column(
+          children: [
+            Container(
+              color: Color.fromARGB(255, 0, 0, 0),
+              height: screenHeight * 0.12,
+              // width: screenWidth,
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(0, 25, 0, 0),
+                child: Row(
+                  children: [
+                    Padding(padding: EdgeInsets.all(20),child: Image.asset('assets/images/logo.png',fit: BoxFit.contain, )),
+                    Padding(padding: EdgeInsets.fromLTRB(20, 0, 0, 0),child: Text("SamaySetu",style: TextStyle(fontFamily: 'title',fontSize:42, color: Color.fromARGB(255, 255, 255, 255)),))
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [Container(
+                    color: Colors.black,
+                    child: Padding(padding:EdgeInsets.fromLTRB(5, 10, 5, 10),
+                      child: 
+                        Text("Bank hours, not money!",textAlign: TextAlign.left,style:TextStyle(color: const Color.fromARGB(107, 255, 255, 255), fontFamily: 'title',fontSize: 40)),
+                        // Spacer(),
+                        
+                      
+                    ),
+                  ),
+                
+                Padding(padding:EdgeInsets.all(10),
+                  child: Container(
+                    height: screenHeight * 0.15,
+                    width:screenWidth *1,
+                    decoration: BoxDecoration(color: const Color.fromARGB(255, 153, 207, 251),
+                    borderRadius: BorderRadius.circular(25),),
+                    
+                    child: Column(mainAxisAlignment: MainAxisAlignment.start,crossAxisAlignment: CrossAxisAlignment.start,
+                      children:[
+                      Padding(padding:EdgeInsets.all(10),child: Text("Your current balance is",style: TextStyle(fontFamily: 'basic',fontSize: 20))),
+                      Row(children:[ 
+                        Padding(padding: EdgeInsets.fromLTRB(20,0,0,0),child: Text(balance.toString(),style:TextStyle(fontWeight: FontWeight.bold,fontSize: 50, color: const Color.fromARGB(255, 0, 84, 152)))),
+                        Spacer(),
+                        Padding(padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+                          child: Container(
+                            decoration:BoxDecoration(shape: BoxShape.circle,border: Border.all(color: Color.fromARGB(107, 255, 255, 255),width: 2)),
+                            child: GestureDetector(onTap: () {
+                              
+                            },
+                            child: CircleAvatar(radius: 35,backgroundColor: Colors.black,child: Padding(padding: EdgeInsets.all(5),child: ClipOval(child: Image.asset("assets/images/logo.png",fit: BoxFit.contain))),)),
+                          ),
+                        )
+                      ])
+                    ])
+                  
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(50),
+                  child:ElevatedButton(
+                    onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>explore()));},  
+                    child: Text('explore')
+                  )
+                ),
+                        
+                Padding( 
+                  padding: EdgeInsets.all(50),
+                  child: ElevatedButton(
+                    onPressed: handleLogout, 
+                    child: Text('logout')
+                  )
+                ),
+                
+                Padding( 
+                  padding: EdgeInsets.all(50),
+                  child: ElevatedButton(
+                    // onPressed: handleMyReq,
+                    onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>myRequests()));}, 
+                    child: Text('My Requests')
+                  )
+                ),
+                        
+                Padding( 
+                  padding: EdgeInsets.all(50),
+                  child: ElevatedButton(
+                    // onPressed: handleMyReq,
+                    onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>reqToMe()));}, 
+                    child: Text('Requests to me')
+                  )
+                ),
+                        
+                ElevatedButton(
+                  onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>editProfile()));}, 
+                  child: Text('edit profile')
+                )
+                          ]
+                        ),
+              ),
+            ),
+      ])
       )
     );
   }
