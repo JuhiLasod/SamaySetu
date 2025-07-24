e9de0d6b67cf1755a3e7b6892d0fecf8c5679e38
JuhiLasod <juhilasod29@gmail.com>
2025-07-24 21:10:14 +0530

    dash page1 done

--- frontend/lib/screens/home.dart
+++ frontend/lib/screens/home.dart
@@ -91,109 +91,170 @@ class _homeState extends State<home>
     final screenHeight= MediaQuery.of(context).size.height;
     final screenWidth= MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(
-        color: Colors.black,
+      body: SingleChildScrollView(
         child: Column(
-          children: [
-            Container(
-              color: Color.fromARGB(255, 0, 0, 0),
-              height: screenHeight * 0.12,
-              // width: screenWidth,
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(0, 25, 0, 0),
-                child: Row(
+          children: [Container(
+            // color: Colors.green,
+            // width: screenWidth*2,
+            height: screenHeight *1,
+            decoration: BoxDecoration(image: DecorationImage(image: AssetImage("assets/images/clock.png", ),fit: BoxFit.cover)),
+            child: Container(color: Colors.black.withAlpha(100),
+              child: Column(
                   children: [
-                    Padding(padding: EdgeInsets.all(20),child: Image.asset('assets/images/logo.png',fit: BoxFit.contain, )),
-                    Padding(padding: EdgeInsets.fromLTRB(20, 0, 0, 0),child: Text("SamaySetu",style: TextStyle(fontFamily: 'title',fontSize:42, color: Color.fromARGB(255, 255, 255, 255)),))
-                  ],
-                ),
-              ),
-            ),
-            Expanded(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [Container(
-                    color: Colors.black,
-                    child: Padding(padding:EdgeInsets.fromLTRB(5, 10, 5, 10),
-                      child: 
-                        Text("Bank hours, not money!",textAlign: TextAlign.left,style:TextStyle(color: const Color.fromARGB(107, 255, 255, 255), fontFamily: 'title',fontSize: 40)),
-                        // Spacer(),
+                    // Container(
+                    //   color: Color.fromARGB(255, 0, 0, 0),
+                    //   height: screenHeight * 0.12,
+                      // width: screenWidth,
+                    //   child: Padding(
+                    //     padding: EdgeInsets.fromLTRB(0, 25, 0, 0),
+                    //     child: Row(
+                    //       children: [
+                    //         Padding(padding: EdgeInsets.all(20),child: Image.asset('assets/images/logo.png',fit: BoxFit.contain, )),
+                    //         Padding(padding: EdgeInsets.fromLTRB(20, 0, 0, 0),child: Text("SamaySetu",style: TextStyle(fontFamily: 'title',fontSize:42, color: Color.fromARGB(255, 255, 255, 255)),))
+                    //       ],
+                    //     ),
+                    //   ),
+                    // ),
+                    Column(
+                      children: [
+                      //   Container(
+                      //   color: Colors.black,
+                      //   child: Padding(padding:EdgeInsets.fromLTRB(5, 10, 5, 10),
+                      //     child: 
+                      //       Text("Bank hours, not money!",textAlign: TextAlign.left,style:TextStyle(color: const Color.fromARGB(107, 255, 255, 255), fontFamily: 'title',fontSize: 40)),
+                      //       // Spacer(),
+                            
+                          
+                      //   ),
+                      // ),
+                    
+                    Padding(padding:EdgeInsets.fromLTRB(10,50,10,0),
+                      child: Container(
+                        height: screenHeight * 0.15,
+                        width:screenWidth *1,
+                        decoration: BoxDecoration(color: const Color.fromARGB(255, 0, 0, 0).withAlpha(150),
+                        borderRadius: BorderRadius.circular(25),),
+                        
+                        child: Row(
+                          children:[
+                            Column(mainAxisAlignment: MainAxisAlignment.start,crossAxisAlignment: CrossAxisAlignment.start,
+                              children:[
+                          Padding(padding:EdgeInsets.fromLTRB(20,20,0,0),child: Text("Your current balance is",style: TextStyle(fontFamily: 'basic',fontSize: 20, color: const Color.fromARGB(255, 127, 127, 127)))),
+                          
+                            Padding(padding: EdgeInsets.fromLTRB(20,0,0,0),child: Text(balance.toString(),style:TextStyle(fontWeight: FontWeight.bold,fontSize: 60, color: Colors.white))),
+                            // Spacer(),
+                            ]),
+                            // Text("💰"),
+                            Spacer(),
+                            Padding(padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+                              child: Container(
+                                // decoration:BoxDecoration(shape: BoxShape.circle,border: Border.all(color: Color.fromARGB(107, 255, 255, 255),width: 2)),
+                                child: GestureDetector(onTap: () {
+                                  
+                                },
+                                child: CircleAvatar(radius: 65,backgroundColor: Colors.transparent,child: Padding(padding: EdgeInsets.all(5),child: ClipOval(child: Opacity( opacity: 0.7,child: Image.asset("assets/images/rupee.png",fit: BoxFit.contain)))),)),
+                              ),
+                            )
+                          
+                        ]),
                         
                       
+                      ),
                     ),
-                  ),
-                
-                Padding(padding:EdgeInsets.all(10),
-                  child: Container(
-                    height: screenHeight * 0.15,
-                    width:screenWidth *1,
-                    decoration: BoxDecoration(color: const Color.fromARGB(255, 153, 207, 251),
-                    borderRadius: BorderRadius.circular(25),),
+                    // Padding(
+                    //   padding: EdgeInsets.all(50),
+                    //   child:ElevatedButton(
+                    //     onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>explore()));},  
+                    //     child: Text('explore')
+                    //   )
+                    // ),
+                            
+                    // Padding( 
+                    //   padding: EdgeInsets.all(50),
+                    //   child: ElevatedButton(
+                    //     onPressed: handleLogout, 
+                    //     child: Text('logout')
+                    //   )
+                    // ),
                     
-                    child: Column(mainAxisAlignment: MainAxisAlignment.start,crossAxisAlignment: CrossAxisAlignment.start,
-                      children:[
-                      Padding(padding:EdgeInsets.all(10),child: Text("Your current balance is",style: TextStyle(fontFamily: 'basic',fontSize: 20))),
-                      Row(children:[ 
-                        Padding(padding: EdgeInsets.fromLTRB(20,0,0,0),child: Text(balance.toString(),style:TextStyle(fontWeight: FontWeight.bold,fontSize: 50, color: const Color.fromARGB(255, 0, 84, 152)))),
-                        Spacer(),
-                        Padding(padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
-                          child: Container(
-                            decoration:BoxDecoration(shape: BoxShape.circle,border: Border.all(color: Color.fromARGB(107, 255, 255, 255),width: 2)),
-                            child: GestureDetector(onTap: () {
-                              
-                            },
-                            child: CircleAvatar(radius: 35,backgroundColor: Colors.black,child: Padding(padding: EdgeInsets.all(5),child: ClipOval(child: Image.asset("assets/images/logo.png",fit: BoxFit.contain))),)),
-                          ),
-                        )
-                      ])
-                    ])
-                  
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(50),
-                  child:ElevatedButton(
-                    onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>explore()));},  
-                    child: Text('explore')
-                  )
-                ),
-                        
-                Padding( 
-                  padding: EdgeInsets.all(50),
-                  child: ElevatedButton(
-                    onPressed: handleLogout, 
-                    child: Text('logout')
-                  )
-                ),
-                
-                Padding( 
-                  padding: EdgeInsets.all(50),
-                  child: ElevatedButton(
-                    // onPressed: handleMyReq,
-                    onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>myRequests()));}, 
-                    child: Text('My Requests')
-                  )
-                ),
-                        
-                Padding( 
-                  padding: EdgeInsets.all(50),
-                  child: ElevatedButton(
-                    // onPressed: handleMyReq,
-                    onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>reqToMe()));}, 
-                    child: Text('Requests to me')
-                  )
-                ),
-                        
-                ElevatedButton(
-                  onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>editProfile()));}, 
-                  child: Text('edit profile')
-                )
-                          ]
-                        ),
-              ),
+                    // Padding( 
+                    //   padding: EdgeInsets.all(50),
+                    //   child: ElevatedButton(
+                    //     // onPressed: handleMyReq,
+                    //     onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>myRequests()));}, 
+                    //     child: Text('My Requests')
+                    //   )
+                    // ),
+                            
+                    // Padding( 
+                    //   padding: EdgeInsets.all(50),
+                    //   child: ElevatedButton(
+                    //     // onPressed: handleMyReq,
+                    //     onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>reqToMe()));}, 
+                    //     child: Text('Requests to me')
+                    //   )
+                    // ),
+                            
+                    // ElevatedButton(
+                    //   onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>editProfile()));}, 
+                    //   child: Text('edit profile')
+                    // )
+                              ]
+                            ),
+                            Padding(padding: EdgeInsets.fromLTRB(10, screenHeight * 0.4, screenWidth * 0.2, 0),child: Text("Bank hours, not money! blahhh blahhhh qwertyu sdfgh xcvbn dfghdfghj wertyui dfghjk",textAlign: TextAlign.left,style:TextStyle(fontWeight: FontWeight.bold , color:  Color.fromARGB(255, 255, 255, 255), fontFamily: 'title',fontSize: 40))),
+                      //      
+                      ]),
             ),
-      ])
+            ),
+            Container(
+              height: screenHeight * 1,
+              color: Colors.black,
+              width: screenWidth*1,
+              child: Column(children:[ 
+                GestureDetector(onTap: (){},child: CircleAvatar(radius: 65,backgroundColor: Colors.transparent,child: Padding(padding: EdgeInsets.all(5),child: ClipOval(child: Opacity( opacity: 0.7,child: Image.asset("assets/images/logo.png",fit: BoxFit.contain)))))),
+                  Padding(
+                    padding: EdgeInsets.all(50),
+                    child:ElevatedButton(
+                      onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>explore()));},  
+                      child: Text('explore')
+                    )
+                  ),
+                          
+                  Padding( 
+                    padding: EdgeInsets.all(50),
+                    child: ElevatedButton(
+                      onPressed: handleLogout, 
+                      child: Text('logout')
+                    )
+                  ),
+                  
+                  Padding( 
+                    padding: EdgeInsets.all(50),
+                    child: ElevatedButton(
+                      // onPressed: handleMyReq,
+                      onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>myRequests()));}, 
+                      child: Text('My Requests')
+                    )
+                  ),
+                          
+                  Padding( 
+                    padding: EdgeInsets.all(50),
+                    child: ElevatedButton(
+                      // onPressed: handleMyReq,
+                      onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>reqToMe()));}, 
+                      child: Text('Requests to me')
+                    )
+                  ),
+                          
+                  ElevatedButton(
+                    onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>editProfile()));}, 
+                    child: Text('edit profile')
+                  )
+              ])
+            )
+        ]),
       )
+      
     );
   }
 }
