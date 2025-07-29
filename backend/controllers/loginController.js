1b86419c6d7ff36f8f4fa47c01ff04916874bab7
JuhiLasod <juhilasod29@gmail.com>
2025-07-29 22:07:55 +0530

    password hashed

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -1,20 +1,27 @@
 import Users from "../models/Users.js";
 import dotenv from "dotenv";
 import jwt from 'jsonwebtoken';
+import bcrypt from "bcryptjs";
 
 dotenv.config();
 const loginController=async(req,res)=>{
     const {email,pass}=req.body;
     console.log(email);
     console.log(pass);
-    const exist=await Users.findOne({email,pass});
-    
-    if(exist)
+    const uexist=await Users.findOne({email});
+    if(uexist)
     {
-        const token=jwt.sign({id: exist._id, email: exist.email},process.env.JWT_SECRET);
-        console.log(token);
-        return res.status(200).json({token});
+        const exist=await bcrypt.compare(pass,uexist.pass);
+        if(exist)
+        {
+            const token=jwt.sign({id: exist._id, email: exist.email},process.env.JWT_SECRET);
+            console.log(token);
+            return res.status(200).json({token});
+        }
     }
+    // const exist=await Users.findOne({email,pass});
+    
+    
     return res.sendStatus(500);
 }
 
